3978f753e3bab50fe4d6386e4c6469da38a807bf
rjassal <rajanram@hotmail.com>
2018-06-08 04:03:46 -0600

    Inital commit with test code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component{
+	render() {return <div>Hello! </div> };
+}
+
+class GoodBye extends React.Component{
+	render() {return <div>GoodBye!</div> };
+}
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+    	<div>
+    		<Route path="/hello" component={Hello}/>
+    		<Route path="/goodbye" component={GoodBye}/>
+    	</div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
